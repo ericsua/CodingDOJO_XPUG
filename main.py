38671774125a6a9aa39011eb3d0e18656137da0d
Riccardo <riccardo.parola@afliant.com>
2025-01-23 20:37:15 +0100

    iteration 3

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ n_items = st.number_input('Number of items', min_value=0, value=0)
 # insert country
 country = st.text_input('Country', 'TX')
 
-discount_df = pd.read_csv('discounts.csv')
+discount_df = pd.read_csv('discounts.csv', index_col='order_value')
 
-taxes_df = pd.read_csv('taxes.csv')
+taxes_df = pd.read_csv('taxes.csv', index_col='state')
+
+discount_df["discount_rate"] = discount_df["discount_rate"].apply(lambda x: f"{(x*100):.2f} %")
+taxes_df["tax_rate"] = taxes_df["tax_rate"].apply(lambda x: f"{(x*100):.2f} %")
+
 
 st.write(discount_df)
 st.write(taxes_df)
