6f054ff51a8c2dd40a16e8ae50cbd1a4e8e12f41
Riccardo <riccardo.parola@afliant.com>
2025-01-23 20:19:10 +0100

    first 3 stories

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import streamlit as st
 
+st.title('Elefant carpaccio')
+
+# Input
+price = st.number_input('Price', min_value=0.0, value=0.0)
+
+n_items = st.number_input('Number of items', min_value=0, value=0)
+
+# Output
+st.write(f'Price: {price*n_items} €')
