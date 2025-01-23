08054ce091f504776a9285dc5ccef0b8f74a80ea
Riccardo <riccardo.parola@afliant.com>
2025-01-23 20:28:55 +0100

    round up 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Elefant carpaccio')
 
@@ -7,5 +8,16 @@ price = st.number_input('Price', min_value=0.0, value=0.0)
 
 n_items = st.number_input('Number of items', min_value=0, value=0)
 
+# insert country
+country = st.text_input('Country', 'TX')
+
+discount_df = pd.read_csv('discounts.csv')
+
+taxes_df = pd.read_csv('taxes.csv')
+
+st.write(discount_df)
+st.write(taxes_df)
+
+
 # Output
-st.write(f'Price: {price*n_items} €')
+st.write(f'Price: {(price*n_items):.2f} $')
